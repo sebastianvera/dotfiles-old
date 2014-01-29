b598a54f4693af6c9f04b14420409ccd517fae05
Sebastian Vera <u.verainf@gmail.com>
2014-01-29 18:20:26 -0300

    Fix symlink errors

--- make.sh
+++ make.sh
@@ -15,17 +15,16 @@ files="vimrc vim zshrc oh-my-zsh gitconfig irbrc"    # list of files/folders to 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "----------------"
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.$file ~/.$file
 done

85e6a398a53cc9e1a9ab1842ba4eede00cf4d8b0
Sebastian Vera <u.verainf@gmail.com>
2014-11-10 11:45:41 -0300

    Add comment to path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,5 +9,5 @@ eval "$(rbenv init - zsh --no-rehash)"
 export GOPATH=$HOME/Code/go
 export PATH=$PATH:$GOPATH/bin
 export PATH="$PATH:/usr/local/opt/go/libexec/bin"
-# binstubs
+# binstubs - keep always at the EOF
 export PATH=".git/safe/../../bin:$PATH"

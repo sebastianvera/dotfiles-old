7f039e75952931514bc985a14f842b9acdac25f3
Sebastian Vera <u.verainf@gmail.com>
2015-11-02 17:35:03 -0300

    Move to neovim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
-alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
 
 # Heroku staging
 alias staging-console='heroku run rails console --remote staging'
@@ -22,16 +22,16 @@ alias db-update-staging='staging restore production'
 alias db-pull='production backup && development restore production'
 
 alias so='source ~/dotfiles/zsh/aliases.zsh'
-alias aliases='vim ~/dotfiles/zsh/aliases.zsh'
-alias zshfunctions='vim ~/dotfiles/zsh/functions.zsh'
+alias aliases='nvim ~/dotfiles/zsh/aliases.zsh'
+alias zshfunctions='nvim ~/dotfiles/zsh/functions.zsh'
 
 alias a='ls -al'
 alias rip="curl -s http://remote-ip.herokuapp.com"
-alias h='heroku'
-alias v='vim'
+alias v='nvim'
+alias v.='v .'
+alias vim='nvim'
 alias gds='git diff --staged'
 alias glogg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative --max-count=30"
 alias glg="g lg"
 alias examen='udp "examen"'
-alias gim='udp Reconomiento\ de\ patrones/Proyecto\ 2/scraper/'
 alias tapas='cd ~/Documents/Ruby/RubyTapas'

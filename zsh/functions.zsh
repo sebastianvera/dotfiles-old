423280a6ec7b803a1f3cc61dbc8504789e701b14
Sebastian Vera <u.verainf@gmail.com>
2016-01-25 14:49:51 -0300

    Add codeclimate function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,3 +19,13 @@ function ips() {
 function editconflicts() { 
   vim +/"<<<<<<<" $( git diff --name-only --diff-filter=U | xargs  )
 }
+
+function codeclimate() {
+  docker run \
+    --interactive --tty --rm \
+    --env CODE_PATH="$PWD" \
+    --volume "$PWD":/code \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume /tmp/cc:/tmp/cc \
+    codeclimate/codeclimate $@
+}
